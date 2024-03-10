4cc682b1fb2cc146e2594d104cb0d4c896d8968f
LesPaulLord <frederikgenereux@gmail.com>
2024-03-10 11:16:42 -0400

    Tweaks to Env + dot reticle added

--- UI/UI_Reticles.gd
+++ UI/UI_Reticles.gd
@@ -1,7 +1,7 @@
 extends Control
 
-@onready var eye_UI = $Eye_Reticle
-@onready var dot_UI = $Dot_Reticle
+@onready var eye_UI = $UI/Eye_Reticle
+@onready var dot_UI = $UI/Dot_Reticle
 
 func looking_at_interactable() :
 	eye_UI.show()
