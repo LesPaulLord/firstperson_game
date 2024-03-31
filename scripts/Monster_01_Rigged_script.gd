83452f1fe9c5c103cedbc3fa26738e8196df839e
LesPaulLord <frederikgenereux@gmail.com>
2024-03-31 09:29:46 -0400

    Work done on Monster_01 animation scripting

--- scripts/Monster_01_Rigged_script.gd
+++ scripts/Monster_01_Rigged_script.gd
@@ -1,5 +1,19 @@
 extends Node3D
 
-func animationPlay():
-	$AnimationTree.active == true
+@onready var at = $AnimationTree
+@onready var cutscenePlayer = $AnimationPlayerCutscene
 
+signal end_MonsterEvent_01
+
+func _ready():
+	var at_mixer = at.get("active")
+	at_mixer = false
+
+func _process(_delta):
+	pass
+
+func animationPlay():
+	cutscenePlayer.play("MainSequence")
+	
+func _on_animation_player_cutscene_animation_finished(Main_Sequence):
+	emit_signal("end_MonsterEvent_01")
